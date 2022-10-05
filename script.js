414e76d049aa479197c1b6b9d2b2f47a35821e12
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-05 16:41:55 -0700

    Added function that saves events to local storage

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var scheduled = ["", "", "", "", "", "", "", "", ""];
+
     // Updates day and time on scheduler 
     var updateCurrent = function() {
         var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
@@ -12,6 +14,7 @@ $(document).ready(function() {
 
     var hour = moment().hour();
     var eventEls = $('div[data-hour]');
+    var containerEl = $(".container");
 
     eventEls.each(function(i, val) {
         var curr = parseInt($(this).attr('data-hour'));
@@ -23,6 +26,21 @@ $(document).ready(function() {
             $(this).attr('class', 'col-md-10 future');
         }
     });
+
+    //figure out
+
+    function saveToStorage() {
+        var button = $(this);
+        var textBoxDiv = button.siblings().eq(1); 
+        var index = parseInt(textBoxDiv.attr('data-hour')) - 9;
+        var input = textBoxDiv.children().first().val(); // value of text box
+
+        scheduled[index] = input;
+
+        localStorage.setItem("scheduled", JSON.stringify(scheduled));
+    }
+
+    containerEl.on('click', '.saveBtn', saveToStorage);
 
 
 
