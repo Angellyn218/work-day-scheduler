8d9c932471d82ea0ed87f874abb2a22f1f4fccfd
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-04 23:28:48 -0700

    Created a function that changes the event's background color based on time

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 JQuery: https://api.jquery.com/
 Bootstrap: https://getbootstrap.com/docs/4.5/getting-started/introduction/
 JQueryUI: https://jqueryui.com/demos/ 
+data-__ type selector: https://stackoverflow.com/questions/4146502/jquery-selectors-on-custom-data-attributes-using-html5 
--- script.js
+++ script.js
@@ -10,6 +10,20 @@ $(document).ready(function() {
     // updates thee initial time and date every second
     setInterval(updateCurrent, 1000);
 
+    var hour = moment().hour();
+    var eventEls = $('div[data-hour]');
+
+    eventEls.each(function(i, val) {
+        var curr = parseInt($(this).attr('data-hour'));
+        if (curr < hour) {
+            $(this).attr('class', 'col-md-10 past');
+        } else if (curr === hour) {
+            $(this).attr('class', 'col-md-10 present');
+        } else {
+            $(this).attr('class', 'col-md-10 future');
+        }
+    });
+
 
 
 }) 
