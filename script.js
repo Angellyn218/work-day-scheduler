57820fa1b00bb0de694b2f5e33ff0a4d2bb71065
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-04 20:50:00 -0700

    Added pictures and text to time blocks

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
+    // Updates day and time on scheduler 
     var updateCurrent = function() {
         var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
         $("#currentDay").text(currentDay)
     }
 
+    // sets the initial time and date
     updateCurrent();
+    // updates thee initial time and date every second
     setInterval(updateCurrent, 1000);
 
 
